d463188ab6945ea4cfe707385cf312160325c47a
Weijie Pan <97200483+weijiePan@users.noreply.github.com>
2024-02-15 18:45:17 +0000

    addition has been put into a function

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-print("this is the addition functionality of the calculator")
-num1 = int(input("Enter a number:"))
-num2 = int(input("Enter another number:"))
-print(num1 + num2)
+#add two num
+def add():
+    print("this is the addition functionality of the calculator")
+    num1 = int(input("Enter a number:"))
+    num2 = int(input("Enter another number:"))
+    print(num1 + num2)
+#divides the larger number by the smaller number
+def divLarBySmall(int num1, int num2)
+
+    if(num1 > num2)
+    
+        return num
+    
+
+
+add()

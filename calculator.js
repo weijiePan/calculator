206d23899c9cc1df5fa03cfe093c78dbb9ed7405
weijiePan <97200483+weijiePan@users.noreply.github.com>
2025-04-03 21:08:10 -0400

    evaluate function implementation

--- calculator.js
+++ calculator.js
@@ -1,31 +1,24 @@
-import {add, multiply, subtract, divide} from "./functions.js"
+import {add, multiply, subtract, divide, pemdas} from "./functions.js"
 class calculator{
     constructor(nksId, oksId, displayTextId){
         this.calculatorContainer = document.getElementById("calculator-container");
         this.displayText = document.querySelector(displayTextId);
+        this.canEnterOperator=false;
     }
     update(change){
         this.displayText.innerText += change;
     }
-    operate(num1, num2, operator){
-        //check if num1, operator, and num2 are filled in order to perform calculation
-        if(num1 && operator && num2){
-            switch(operator){
-                case "+":
-                    return add(num1,num2);
-                case "-":
-                    return subtract(num1, num2);
-                case "*":
-                    return multiply(num1, num2);
-                case "/":
-                    return divide(num1, num2);
-            }
+    evaluate() {
+        //checks calculation parts form valid expression
+        if(Number(this.displayText.innerText.at(this.displayText.innerText.length - 1))){
+            let result = pemdas(this.displayText.innerText);
+            this.displayText.innerText = result? result:this.displayText.innerText;
+        }else{
+            alert("invalid input")
         }
-        
     }
     reset(){
         this.displayText.innerText = "";
-
     }
 }
 
@@ -34,7 +27,16 @@ let calc = new calculator("numbers", "operators", "#display p")
 calc.calculatorContainer.addEventListener("mousedown", function(e){
     if(Number(e.target.id.at(1))){//check if a number or operator key is pressed
         calc.update(e.target.id.at(1))
+        calc.canEnterOperator = true;
     }else if(/[\+\-\/\*]/.test(e.target.id)){
-        calc.update(e.target.id);
+        if(calc.canEnterOperator){//checks that operators are only between numbers
+            calc.update(e.target.id);
+            calc.canEnterOperator = false;
+        }else{
+            alert("invalid");
+        }
+        
+    }else if(/=/.test(e.target.id)){
+        calc.evaluate();
     }
 })
--- functions.js
+++ functions.js
@@ -11,15 +11,38 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
-    let result = num1/num2;
-    //rounds decimal number if decimal place is more than 2
-    if(result * 100 % 1 !== 0){
-        return Math.trunc(result * 100)/100;
-    }else{
-        return result;
+    return Math.round(num1/num2 * 100)/100;
+}
+
+function pemdas(s){
+    if(s.search(/\*/) != -1){
+        return(retResult(s.search(/\*/), "m", s));
+    }else if(s.search(/\//) != -1){
+        return(retResult(s.search(/\//), "d", s));
+    }else if(s.search(/\+/) != -1){
+        return(retResult(s.search(/\+/), "a", s));
+    }else if(s.search(/\-/) != -1){
+        return(retResult(s.search(/\-/), "s", s));
+    }
+}
+function retResult(i, mode, s){// m *, a + , s -, d /
+    let l = i - 1, r = i + 1 
+    while (l > 0 && !/[\+\-\*\/]/.test(s[l])){//find start of left number
+        l -= 1;
+    }
+    while (r < s.length && !/[\+\-\*\/]/.test(s[r])){//find end of right number
+        r += 1;
+    }
+    switch(mode){
+        case "m":
+            return s.slice(0, l) + multiply(Number(s.slice(l,i)), Number(s.slice(i+1, r))) + s.slice(r);
+        case "d":
+            return s.slice(0, l) + divide(Number(s.slice(l, i)), Number(s.slice(i+1, r))) + s.slice(r);
+        case "a":
+            return s.slice(0, l) + add(Number(s.slice(l, i)), Number(s.slice(i+1,r))) + s.slice(r);
+        case "s":
+            return s.slice(0, l) + subtract(Number(s.slice(l,i)), Number(s.slice(i+1,r))) + s.slice(r);
     }
 }
 
-
-
-export {add, subtract, multiply, divide}
+export {add, subtract, multiply, divide, pemdas}
